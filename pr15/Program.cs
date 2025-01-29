c941966be9330d357979181ccf2af6e2265d0510
ИСП-22 <-22@LAB30-05>
2025-01-29 19:23:09 +0300

    +pr15

---
+++ pr15/Program.cs
@@ -0,0 +1,8 @@
+﻿using NumberLibrary; // Подключаем пространство имен библиотеки
+
+int number = 1234;
+int divider = 6;
+
+Console.WriteLine($"Равны ли суммы первых двух и последних двух цифр? {NumberAnalyzer.SumOfFirstTwoDigits(number)}");
+Console.WriteLine($"Кратна ли сумма цифр трём? {NumberAnalyzer.IsSumDivisibleByThree(number)}");
+Console.WriteLine($"Кратно ли произведение цифр числу {divider}? {NumberAnalyzer.IsProductDivisibleByA(number, divider)}");
