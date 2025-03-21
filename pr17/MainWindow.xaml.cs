f1c4e2f6c3731cb19fc7d0becd7e13b153e11d0e
seaferz <xxxchebmakxxx@gmail.com>
2025-03-21 16:20:36 +0300

    add17

---
+++ pr17/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr17;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Calculate(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(textBoxNumberFirst.Text, out double a) &&
+                double.TryParse(textBoxNumberSecond.Text, out double b) &&
+                    double.TryParse(textBoxNumberTh.Text, out double c))
+        {
+            double volume = a * b * c;
+            double surfaceArea = 2 * (a * b + b * c + a * c);
+
+            textBoxNumberResult.Text = $"Объем: {volume}\nПлощадь поверхности: {surfaceArea}";
+        }
+
+        else
+        {
+            textBoxNumberResult.Text = "Error";
+        }
+                
+    }
+    private void AboutProgram(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Чеботарев Максим ИСП-22\nДаны длины ребер a, b, c прямоугольного параллелепипеда. \nНайти его объем V = a·b·c и площадь поверхности\n S = 2·(a·b + b·c + a·c).");
+    }
+
+    private void Exit(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+
+}
